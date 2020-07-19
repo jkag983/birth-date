2678abdb7cc5a206eb6d169f2b2cb9e361053d3c
Joe Kaguchu <kaguchumagua@gmail.com>
2020-07-19 23:30:04 +0300

    control flows added in script.js file

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,41 @@ var month=parseInt(document.getElementById('mon:')).innerHTML=mon;
 var day=parseInt(document.getElementById('day:')).innerHTML=day;
 var result=DOB(century, year, month, day)%7;
 alert(result);
+
+//let sunday=0
+//let monday=1
+//let tuesday=2
+//let wednesday=3
+//let thursday=4
+//let friday=5
+//let saturday=6
+
+if ((result <1) && (gender ===male)) {
+  name="Kwasi"
+} else if ((result<1) &&(gender ===female)) {
+    name="Akosua"
+} else if ((result<2) &&(gender===male)) {
+    name="Kwadwo"
+} else if ((result<2) &&(gender==female)) {
+    name="Adwoa"
+} else if ((result<3) &&(gender===male)) {
+    name="kwabena"
+} else if ((result<3) &&(gender===female)) {
+    name="Abenna"
+} else if ((result<4) &&(gender===male)) {
+    name="Kwaku"
+} else if ((result<4) &&(gender===female)) {
+    name="Akua"
+} else if ((result<5) &&(gender===male)) {
+    name="Yaw"
+} else if ((result<5) &&(gender===female)) {
+    name="Yaa"
+} else if ((results<6) &&(gender===male)) {
+    name="Kofi"
+} else if ((result<6) &&(gender===female)) {
+    name="Afua"
+} else if ((result<7) &&(gender===male)){
+    name="Kwame"
+} else ((result<7) &&(gender===female)) {
+    name="Ama"
+}
