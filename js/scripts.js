83705295e07081ede46c58c80a1c0635f595d6dd
Joe Kaguchu <kaguchumagua@gmail.com>
2020-07-20 00:42:13 +0300

    js script code changed

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 var DOB=function(century, year, month, day){
 return ((century/4)-2*century-1)+(5*year/4)+(26*(month+1)/10)+day;
 };
-var century=parseInt(document.getElementById('century:')).innerHTML=century;
-var year=parseInt(document.getElementById('year:')).innerHTML=year;
-var month=parseInt(document.getElementById('mon:')).innerHTML=mon;
-var day=parseInt(document.getElementById('day:')).innerHTML=day;
+var century=parseInt(prompt('century:'));
+var year=parseInt(prompt('year:'));
+var month=parseInt(prompt('month:')).;
+var day=parseInt(promt('day:'));
 var result=DOB(century, year, month, day)%7;
 alert(result);
 
