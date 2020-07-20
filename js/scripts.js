c5136ad0fb2e20ad82d3a6bc0104ce7588f83eb8
Joe Kaguchu <kaguchumagua@gmail.com>
2020-07-20 15:46:21 +0300

    index,html modified

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
-function myFunction() {
-  var d = new Date();
-  var n = d.getDay()
-  document.getElementById("demo").innerHTML = n;
+function DOB(){
+var d=myDate(prompt("Date of birth"));
+var result=d.getday();
+var day ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+
+
+document.getElementById("Date of birth").innerHTML = "Your Ghananian name is" + result ".";
 };
 //let sunday=0
 //let monday=1
@@ -10,33 +13,3 @@ function myFunction() {
 //let thursday=4
 //let friday=5
 //let saturday=6
-
-if ((result <1) && (gender ===male)) {
-  name="Kwasi"
-} else if ((result<1) &&(gender ===female)) {
-    name="Akosua"
-} else if ((result<2) &&(gender===male)) {
-    name="Kwadwo"
-} else if ((result<2) &&(gender==female)) {
-    name="Adwoa"
-} else if ((result<3) &&(gender===male)) {
-    name="kwabena"
-} else if ((result<3) &&(gender===female)) {
-    name="Abenna"
-} else if ((result<4) &&(gender===male)) {
-    name="Kwaku"
-} else if ((result<4) &&(gender===female)) {
-    name="Akua"
-} else if ((result<5) &&(gender===male)) {
-    name="Yaw"
-} else if ((result<5) &&(gender===female)) {
-    name="Yaa"
-} else if ((results<6) &&(gender===male)) {
-    name="Kofi"
-} else if ((result<6) &&(gender===female)) {
-    name="Afua"
-} else if ((result<7) &&(gender===male)){
-    name="Kwame"
-} else ((result<7) &&(gender===female)) {
-    name="Ama"
-}
