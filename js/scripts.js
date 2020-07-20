279a643fc45e80a1afa75f059d217c5cc1c3a6a8
Joe Kaguchu <kaguchumagua@gmail.com>
2020-07-21 02:03:16 +0300

    body in html modified

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,11 @@
-function DOB(){
-var d=myDate(prompt("Date of birth"));
-var result=d.getday();
-var day ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+function dateOfBirth() {
+var dateOfBirth =parseInt(prompt("what is your date of birth .../.../...?"));
+var d= new Date(dateOfBirth);
+var n =d.getDay();
+var h2 = document.createElement("h2");
+var resultText = document.createTextNode("Your name is" + n + "!");
+h2.setAttribute("id", "dateOfBirth");
+h2.appendChild(resultText);
+document.getElementById("result").appendChild(h2);
 
-
-document.getElementById("Date of birth").innerHTML = "Your Ghananian name is" + result ".";
-};
-//let sunday=0
-//let monday=1
-//let tuesday=2
-//let wednesday=3
-//let thursday=4
-//let friday=5
-//let saturday=6
+}
