d85fc5c502ef3ccd75aadea2e2622940bc91ee04
Joe Kaguchu <kaguchumagua@gmail.com>
2020-07-21 02:25:05 +0300

    script modified

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,12 @@ function dateOfBirth() {
 var dateOfBirth =parseInt(prompt("what is your date of birth .../.../...?"));
 var d= new Date(dateOfBirth);
 var n =d.getDay();
-var h2 = document.createElement("h2");
+var h3 = document.createElement("h3");
 var resultText = document.createTextNode("Your name is" + n + "!");
-h2.setAttribute("id", "dateOfBirth");
-h2.appendChild(resultText);
-document.getElementById("result").appendChild(h2);
+h3.setAttribute("id", "dateOfBirth");
+h3.appendChild(resultText);
+document.getElementById("result").appendChild(h3);
+
+
 
 }
