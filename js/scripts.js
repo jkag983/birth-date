6acce118b88f3478e0b20ba467d5166fdc7fa7e0
Joe Kaguchu <kaguchumagua@gmail.com>
2020-07-21 03:58:40 +0300

    index.html modified

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,22 @@ var date =parseInt(prompt("what is your date of birth .../.../...?"));
 var dateOfBirth= new Date(date);
 var days=dateOfBirth.getDay();
 var h3 = document.createElement("h3");
-var resultText = document.createTextNode("Your name is" + days + "!");
+var resultText = document.createTextNode("Your day of week is " + days + "!");
 h3.setAttribute("id", "dateOfBirth");
 h3.appendChild(resultText);
 document.getElementById("result").appendChild(h3);
-
-
-
 }
 
-//sunday===0
-//monday===1
-//tuesday===2
-//wednesday===3
-//Thursday===4
-//Friday===5
-//Saturday===6
+
+
+
+
+
+//          female       male
+//sunday===Akosua        Kwasi
+//monday===Adwoa         Kwadwo
+//tuesday===Abenna       kwabena
+//wednesday===Akua       Kwaku
+//Thursday===Yaa         Yaw
+//Friday===Afua          Kofi
+//Saturday===Ama         Kwame
